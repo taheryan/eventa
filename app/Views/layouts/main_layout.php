8884202298659f7320f8d06383b506d77f7f04a7
Erfan Taheryan <erfantaheryan@gmail.com>
2024-11-11 13:43:36 +0330

    init

---
+++ app/Views/layouts/main_layout.php
@@ -0,0 +1,31 @@
+<!-- app/Views/layouts/main_layout.php -->
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $this->renderSection('title') ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="http://localhost/tickr/public/style.css" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <h1>سامانه پشتیبانی</h1>
+    <?php if(session()->get('username')): ?>
+    <p>خوش آمدی <?= session()->get('username') ?> (<?= session()->get('role') ?>) جان!
+    <?php endif; ?>
+    <nav>
+        <a href="http://localhost/eventa/public/tickets">تیکت ها</a> | <a href="http://localhost/tickr/public/auth/signup">ثبت نام</a> | <a href="/about">درباره ما</a> | <a href="http://localhost/tickr/public/auth/logout">خروج</a>
+    </nav>
+</header>
+
+<main>
+    <?= $this->renderSection('content') ?>
+</main>
+
+<footer>
+    <p>&copy; <?= date('Y') ?> سامانه پشتیبانی</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
